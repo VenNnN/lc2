17b941a6d55de720fb3317543872e5e634e35168
Veniamin Nelin <veniamin.nelin@gmail.com>
2022-10-20 23:44:22 +0300

    func2 complete

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ def acquaintance():
 	print("Good! I am ", names.get_full_name())
 	pass
 
+def dogName():
+	input("What is name your dog? ")
+	print("Good! My dog name is ", names.get_first_name())
+	pass
+
 acquaintance()
+dogName()
